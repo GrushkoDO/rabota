bc6a897e86a604c17ac58fc0d79606efba490920
олина попа < @DESKTOP-HI2AFKM>
2022-11-08 11:16:18 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public delegate void AccountHandler(string message);
+    public class Account
+    {
+        int sum;
+        string FIO;
+        AccountHandler taken;
+        public Account(int sum, string a)
+        {
+            this.sum = sum;
+            FIO = a;
+
+        }
+        // Регистрируем делегат
+        public void RegisterHandler(AccountHandler del)
+        {
+            taken += del;
+        }
+  
+        public void Add(int sum)
+        {
+            this.sum += sum;
+            taken?.Invoke($" На счет зачисленно {sum}  баланс {this.sum}");
+        }
+        public void registr()
+        {
+            taken?.Invoke($"Аккаунт под именем{FIO} и с балансом {sum} был зареган");
+        }
+
+        public void Take(int sum)
+        {
+            if (this.sum >= sum)
+            {
+                this.sum -= sum;
+                taken?.Invoke($"Со счета списано {sum}  остаток {this.sum}");
+            }
+            else
+                taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
+        }
+    }
+    public delegate void Grob(string message);
+    public class bober
+    {
+        int kassa;
+        string ima;
+        event Grob robbery;
+        public bober(int a, string b)
+        {
+            kassa = a;
+            ima = b;
+        }
+        public void RegisterHandler(Grob del)
+        {
+            robbery += del;
+        }
+        public void Add(int sum)
+        {
+            kassa += sum;
+            robbery.Invoke($" На счет зачисленно {sum}  баланс {kassa}");
+        }
+        public void registr()
+        {
+            robbery?.Invoke($"Аккаунт под именем{ima} и с балансом {kassa} был зареган");
+        }
+
+        public void Take(int sum)
+        {
+            if (this.kassa >= sum)
+            {
+                this.kassa -= sum;
+                robbery?.Invoke($"Со счета списано {sum}  баланс {kassa}");
+            }
+            else
+                robbery?.Invoke($"Недостаточно средств. Баланс: {kassa} баланс{kassa}");
+        }
+    }
+}
---
+++ rabota/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace rabota
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Account PB;
+        public void Regist(string mes)
+        {
+            listBox1.Items.Add(mes);
+        }
+        public void kley(string mes)
+        {
+            MessageBox.Show(mes);
+        }
+        bober JL;
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PB = new Account(Convert.ToInt32(textBox2.Text),textBox1.Text);
+            PB.RegisterHandler(Regist);
+            PB.registr();
+            JL= new bober(Convert.ToInt32(textBox2.Text), textBox1.Text);
+            JL.RegisterHandler(kley);
+            JL.registr();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PB.Add(Convert.ToInt32(textBox3.Text));
+            JL.Add(Convert.ToInt32(textBox3.Text));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            PB.Take(Convert.ToInt32(textBox4.Text));
+            JL.Take(Convert.ToInt32(textBox4.Text));
+        }
+    }
+}
